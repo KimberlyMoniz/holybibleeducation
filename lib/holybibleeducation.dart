22ee198cb248c2c6c7f7037e259799bf36dd63dc
KimberlyMoniz <kimberlythemoniz@gmail.com>
2023-06-14 13:30:07 +0530

    FR1

---
+++ lib/holybibleeducation.dart
@@ -0,0 +1,28 @@
+library holybibleeducation;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+class Firstreading{
+
+  String fr_14062023 = '''
+  Such is the confidence that we have through Christ toward God. 
+  Not that we are competent of ourselves to claim anything as coming from us; 
+  our competence is from God, who has made us competent to be ministers of a new covenant, 
+  not of letter but of spirit; for the letter kills, but the Spirit gives life.
+  Now if the ministry of death, 
+  chiseled in letters on stone tablets, 
+  came in glory so that the people of Israel could not gaze at Moses' face 
+  because of the glory of his face, a glory now set aside, 
+  how much more will the ministry of the Spirit come in glory? 
+  For if there was glory in the ministry of condemnation, 
+  much more does the ministry of justification abound in glory! 
+  Indeed, what once had glory has lost its glory because of the greater glory; 
+  for if what was set aside came through glory, much more has the permanent come in glory!
+
+  Since, then, we have such a hope, we act with great boldness.''';
+
+}
